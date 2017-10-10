f4e20344d170cbae5aa0f93dc630096472ae1183
Fariz Luqman <fariz.fnb@gmail.com>
2017-10-10 02:51:39 +0000

    Apply fixes from StyleCI

--- errorpage/simple.php
+++ errorpage/simple.php
@@ -1,7 +1,7 @@
 <html>
 <head>
 </head>
-<style type="text/css"><?php include('style.css'); ?></style>
+<style type="text/css"><?php include 'style.css'; ?></style>
 <div class="dss_main">
 	<div class="dss_error">
 		<h1><b>Error:</b> <?= $message ?></h1>
